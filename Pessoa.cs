29aeef12d98ad3c62488d159daaabe0031fc88ba
Karoline Suzuki <42789906+suzukikarol@users.noreply.github.com>
2018-10-20 15:32:39 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace meuProjeto.Dominio
+{
+    class Pessoa
+    {
+
+        public Pessoa(string nome, string profissao)
+        {
+            Nome = nome;
+            Profissao = profissao;
+        }
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Profissao { get; set; }
+        public char Genero { get; set; }
+    }
+}
